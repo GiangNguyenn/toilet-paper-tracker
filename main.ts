4e8ffc3b1f223c1b154de34560462eb689edb362
Giang Nguyen <68680776+GiangNguyenn@users.noreply.github.com>
2020-12-10 09:38:21 +0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,11 @@
+let counter = 0
+input.onPinPressed(TouchPin.P0, function () {
+        counter += 1
+})
+basic.forever(function () {
+    if(counter < 3){
+        basic.showNumber(counter)
+        } else if( counter === 3) {
+            basic.showIcon(IconNames.No)
+        }
+})
