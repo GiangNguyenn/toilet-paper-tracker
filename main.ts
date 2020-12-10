6f71899dd6a254d2fa4638f70e845b3ffa23bbec
Giang Nguyen <68680776+GiangNguyenn@users.noreply.github.com>
2020-12-10 10:06:14 +0700

    add repeat

--- main.ts
+++ main.ts
@@ -1,11 +1,13 @@
 let counter = 0
 input.onPinPressed(TouchPin.P0, function () {
-        counter += 1
+    counter += 1
 })
 basic.forever(function () {
-    if(counter < 3){
+    if (counter < 3) {
         basic.showNumber(counter)
-        } else if( counter === 3) {
-            basic.showIcon(IconNames.No)
-        }
+    } else if (counter == 3) {
+        basic.showIcon(IconNames.No)
+        pause(30000)
+        counter = 0
+    }
 })
